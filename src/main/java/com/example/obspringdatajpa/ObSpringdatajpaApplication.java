494602247eb65e121a7f82c0df151e74c91f288e
duvan1026 <duvancastro1026@gmail.com>
2023-03-28 10:24:02 -0500

    update file

--- src/main/java/com/example/obspringdatajpa/ObSpringdatajpaApplication.java
+++ src/main/java/com/example/obspringdatajpa/ObSpringdatajpaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ObSpringdatajpaApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ObSpringdatajpaApplication.class, args);
 	}
 
